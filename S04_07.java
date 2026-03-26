2a9f2a0e6ca3cb85f852219d875b07247258f675
sinwonjun <daejung1625@dge.go.kr>
2026-03-26 13:56:33 +0900

    Add files via upload

---
+++ S04_07.java
@@ -0,0 +1,17 @@
+package jump2java;
+
+public class S04_07 {
+	public static void main(String[] args) {
+		int a = 0;
+		while (a < 10) {
+			a++;
+			if (a % 2 != 0) {
+			continue;
+		
+	}
+	System.out.println(a);
+	
+
+}
+	}
+}
---
+++ S04_08.java
@@ -0,0 +1,5 @@
+package jump2java;
+
+public class S04_08 {
+
+}
